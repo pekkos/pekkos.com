469927d1dbfec0257d2cb169125cb568f000aaf5
Jonas Päckos <jpackos@gmail.com>
2019-09-06 09:48:57 +0200

    git ignore _site

--- gulp/copy.js
+++ gulp/copy.js
@@ -38,7 +38,7 @@ module.exports = function () {
     return gulp.src(['src/css/*.min.css'])
       .pipe(copy('_site/assets/css', { prefix: 4 }))
       .on('end', function () {
-        console.log('CSS files copied to Fractal Styleguide.')
+        console.log('CSS files copied to Eleventy site.')
       });
   });
 
